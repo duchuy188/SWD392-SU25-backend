58c5623387c0c91e76f1747b3ddeea87044d4b40
edentuan <131292714+edentuan@users.noreply.github.com>
2025-06-14 21:07:45 +0700

    Update server.js

--- AI Chatbot - BE/server.js
+++ AI Chatbot - BE/server.js
@@ -16,6 +16,8 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 app.use(express.json());
